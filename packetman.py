b9f857c84dd8d20777d9f33d74f7552bbd6ea96b
alexB15 <abrown15@uoregon.edu>
2019-04-15 14:33:15 -0700

    Implemented/Revised static type checking

--- packetman.py
+++ packetman.py
@@ -1,26 +1,26 @@
 import subprocess
 
 #Read bash file and insert location in ping and traceroute commands
-str target = "google.com"
+target: str = "google.com"
 
 subprocess.call("./tracert.sh", shell=True)
 
 #Read ping.sh and replace locations where appropriate
 #Get local from traceroute.txt (first hop)
-str local = "8.8.8.8"
+local: str = "8.8.8.8"
 
 subprocess.call("./ping.sh", shell=True)
 
 localPings = {}
 targetPings = {}
 
-int maxLocal = max(localpings)
-int minLocal = min(localPings)
-int avgLocal = sum(localPings)/len(localPings)
+maxLocal: int = max(localPings)
+minLocal: int = min(localPings)
+avgLocal: int = sum(localPings)/len(localPings)
 
-int maxTarget = max(targetPings)
-int minTarget = min(targetPings)
-int avgTarget = sum(targetPings)/len(targetPings)
+maxTarget: int = max(targetPings)
+minTarget: int = min(targetPings)
+avgTarget: int = sum(targetPings)/len(targetPings)
 
 #THREAD 1: Read localping.txt and targetping.txt, retreive minimum, maximum and average ping for each
 
