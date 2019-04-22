d9ea678597c8c61f8d2b761160dbc859caf7a433
Noah Rose <nrose@uoregon.edu>
2019-04-22 13:55:41 -0700

    Added some threading, modified bash scripts

--- packetman.py
+++ packetman.py
@@ -1,26 +1,60 @@
 import subprocess
+import threading
 
 #Read bash file and insert location in ping and traceroute commands
-target: str = "google.com"
-
-subprocess.call("./traceroute.sh")
+target = "google.com"
 
 #Read ping.sh and replace locations where appropriate
 #Get local from traceroute.txt (first hop)
-local: str = "8.8.8.8"
-
-subprocess.call("./ping.sh")
 
 localPings = {}
 targetPings = {}
 
-maxLocal: int = max(localPings)
-minLocal: int = min(localPings)
-avgLocal: int = sum(localPings)/len(localPings)
+#Update the statistics for the ping sets
+def UpdateStates ():
+    maxLocal = max(localPings)
+    minLocal = min(localPings)
+    avgLocal = sum(localPings)/len(localPings)
 
-maxTarget: int = max(targetPings)
-minTarget: int = min(targetPings)
-avgTarget: int = sum(targetPings)/len(targetPings)
+    maxTarget = max(targetPings)
+    minTarget = min(targetPings)
+    avgTarget = sum(targetPings)/len(targetPings)
+
+#Run data collection processes
+def DataCollect ():
+    subprocess.run("./traceroute.sh", shell=True, check=True)
+    #Get local
+    #Set local
+    local = "8.8.8.8"
+    subprocess.run("./ping.sh", shell=True, check=True)
+    print("Data collected!")
+
+def Clean ():
+    print ("Cleaning...")
+    
+def GetKill ():
+    user_in = str (input ("Enter 'K' to kill: "))
+    while (user_in != 'K'):
+       user_in = str (input ("Enter 'K' to kill: "))
+
+#Main 
+if __name__ == "__main__":
+    collectionThread = threading.Thread(target=DataCollect)
+    collectionThread.start()
+    collectionThread.join()
+    
+    cleanupThread = threading.Thread(target=Clean)
+    cleanupThread.start()
+    
+    killThread = threading.Thread(target=GetKill)
+    killThread.start()
+    killThread.join()
+
+
+
+
+
+
 
 #THREAD 1: Read localping.txt and targetping.txt, retreive minimum, maximum and average ping for each
 
--- ping.sh
+++ ping.sh
@@ -1,2 +1,3 @@
-ping -i .200 8.8.8.8 > localping.txt
-ping -i .200 8.8.8.8 > targetping.txt
+#!/bin/bash
+ping -i .200 -c 100 8.8.8.8 > localping.txt
+ping -i .200 -c 100 8.8.8.8 > targetping.txt
