a77c44adf50b9ecf079ca78bffba53f9c080db3d
Alex Brown <49386016+alexB18@users.noreply.github.com>
2019-06-25 18:15:42 -0700

    Removed cowsay for portability :(

--- ping.sh
+++ ping.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
- cowsay 'Pinging Website and Recording data... this may take a while'
+ echo 'Pinging Website and Recording data... this may take a while\n'
 ping -i .200 -c 20 10.252.10.54 > localping.txt
  ping -i .200 -c 20 google.com > targetping.txt
